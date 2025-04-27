c3fc50153757c992fd7f685e89ed25555f09ec4e
Shuru63 <shubhamkuargarg63@gmail.com>
2025-04-27 18:09:11 +0530

    add the all things and complete projct

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,11 @@ import Fav from './pages/Fav/Fav'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
-
+import ItemDisplay from './components/ItemDisplay/ItemDisplay'
+import ProductDetails from './components/ProductDetails/ProductDetails'
+import ContactPage from './components/ContactPage/ContactPage'
 const App = () => {
-
+const [category,setCategory] = useState("All");
   const[showLogin,setShowLogin]=useState(false)
   return (
     <>
@@ -18,6 +20,9 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/fav' element={<Fav />} />
+          <Route path='/product' element={<ItemDisplay category={category}/>} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
           <Route path='/order' element={<PlaceOrder />} />
         </Routes>
       </div>
